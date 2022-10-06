0b2a1fc7066335e41bfaa8060ff231802d261688
André Souza <andresouza@meraki.dev.br>
2022-10-06 11:31:08 -0300

    [ADD] Configuration pixel facebook.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('577598564117202') // facebookPixelId
+        ReactPixel.pageView()
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, []);
   return (
     <>
       <Component {...pageProps} />
